a9efda628aaade043e813fbd9b466f3d67ecfa26
mt-repository <dummy@dummydummyabc123xyz.fake>
2013-12-23 20:03:53 +0900

    Initial Commit

---
+++ dummy7Tests/dummy7Tests.m
@@ -0,0 +1,34 @@
+//
+//  dummy7Tests.m
+//  dummy7Tests
+//
+//  Created by mtanaka on 2013/12/23.
+//  Copyright (c) 2013年 mtanaka. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface dummy7Tests : XCTestCase
+
+@end
+
+@implementation dummy7Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
